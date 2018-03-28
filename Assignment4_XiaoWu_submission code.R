514fd6b9dea19eab9f1fa050e55c5ad587a8937e
XiaoWu <34700197+EchoWOO@users.noreply.github.com>
2018-03-28 15:55:34 -0700

    Update
    Add another dataset and a little bit housecleaning

--- Assignment4_XiaoWu_submission code.R
+++ Assignment4_XiaoWu_submission code.R
@@ -20,9 +20,7 @@ library(ROAuth)
 #https://cran.r-project.org/web/packages/streamR/streamR.pdf
 
 library(data.table)
-
-
-emoji("thumbsup")
+#emoji("thumbsup")
 
 #define plot theme
 plotTheme <- function(base_size = 24) {
@@ -47,43 +45,12 @@ plotTheme <- function(base_size = 24) {
 )
 }
 
-# Matching a single string against a single keyword
-#   If regexpr finds a match, it will return the character position at which the match is found (in the example above, it will return 1).
-#   If it does not find a match, regexpr will return -1.
-
-keyword <- "<ed><a0><bd><ed><b1><8d>" # encoding of the "thumbs up emoji"
-tweetText <- "<ed><a0><bd><ed><b1><8d>nothing is impossible https://t.co/AckwyTxVjR"
-
-results <- regexpr(keyword,tweetText)
-
-##possible solutions for taking out the 
-#take out the emoji code
-str_replace()
-#regex alphanumeric, any alpha numeric characters between < and >
-reg <- "<[a-zA-Z0-9][a-zA-Z0-9]>"
-tweetText <- streamedtweets
-reggsub(reg," ",tweetText)#(what to look for, what to replace, input string)
-
-
 # Matching text with Emoji stream API encoding
 emojiEncodings <- read.csv("D:/620/MUSA-620-Week-9-master/MUSA-620-Week-9-master/MUSA-620-Week-9-master/MUSA-620-Week-9-master/emoji-encodings.csv")
-tweetTexts <- c("<ed><a0><bd><ed><b1><8d>nothing is impossible https://t.co/AckwyTxVjR","Yesterday was a huge day for our founders <e2><9c><8d><ef><b8><8f><ed><a0><be><ed><b7><a0><ed><a0><bd><ed><b2><a5> Thanks to the @MYOB and @BlueChilliGroup marketing and social media specialis<e2><80><a6>")
 
 resultsofemoji <- sapply(emojiEncodings$streamApiEncodings, regexpr, streamedtweetstext) %>%
   data.frame()
 colnames(resultsofemoji) <- emojiEncodings$emojiDescription
-
-##Take out all the tweet text with Emoji US
-resultsUS <- resultsofemoji["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S"] %>%
-  cbind(streamedtweetstext) %>%
-  setnames(old=c("REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S","streamedtweetstext"), new=c("detectresult", "tweet")) %>%
-  mutate(detectresult = as.numeric(detectresult),
-         tweet = as.character(tweet)) %>%
-  dplyr::select(detectresult,tweet) %>%
-  as.data.frame() %>%
-  dplyr::filter(detectresult!= -1) %>%
-  dplyr::select(tweet)
-  
 
 # STREAMING API 
 #My key
@@ -118,7 +85,7 @@ my_oauth <- oathfunction(consumer_key, consumer_secret, access_token, access_sec
 # Keep in mind that most of these parameters will not work together
 # For example, the location search cannot be paired with other parameters
 
-file = "mytwitterstreamchina.json"       #The data will be saved to this file as long as the stream is running
+file = "mytwitterstreamchina1.json"       #The data will be saved to this file as long as the stream is running
 track = c("China") # c("#maga")                 #"Search" by keyword(s)
 follow = NULL #c("pepsi","cocacola","7up")
 #"Search" by Twitter user(s)
@@ -126,7 +93,7 @@ follow = NULL #c("pepsi","cocacola","7up")
 loc = NULL
 lang = NULL                             #Filter by language
 timeout = NULL #1000                          #Maximum time (in miliseconds)
-tweets = 30000 #1000                      #Maximum tweets (usually, it will be less)
+tweets = 50000 #1000                      #Maximum tweets (usually, it will be less)
 
 
 filterStream(file.name = file, 
@@ -139,31 +106,33 @@ filterStream(file.name = file,
              oauth = my_oauth,
              verbose = TRUE)
 
-streamedtweets <- parseTweets(file, verbose = FALSE)
+###############For Key word "China"######################
+streamedtweets <- parseTweets("mytwitterstreamchina.json", verbose = FALSE)
+streamedtweets1 <- parseTweets("mytwitterstreamchina1.json", verbose = FALSE)
 
 # IMPORTANT - for the emojis to be properly encoded, you must use the line below
 streamedtweets$text <- iconv(streamedtweets$text , from = "latin1", to = "ascii", sub = "byte")
+streamedtweets1$text <- iconv(streamedtweets1$text , from = "latin1", to = "ascii", sub = "byte")
+
+streamedtweetschina <- rbind(streamedtweets,streamedtweets1)
 
 # OPTIONAL - If you want to use the REST API (rtweet package) in your analysis, you should use this command to encode the emojis
 restAPI$text <- iconv(restAPI$text , from = "latin1", to = "ascii", sub = "byte")
 
 #filter the retweets
-streamedtweets <- streamedtweets %>%
+streamedtweetschina <- streamedtweetschina %>%
   filter(!str_detect(text, '^"')) %>%
   filter(!str_detect(text, '^RT'))
 
 #Get the text of retweets 
-streamedtweetstext <- streamedtweets$text
-streamedtweet1<- dplyr::select(streamedtweets, source, text, created_at)
+streamedtweetschinatext <- streamedtweetschina$text
+streamedtweetschina<- dplyr::select(streamedtweets, source, text, created_at)
 
 # Matching multiple strings against a multiple keywords
 
-emojiEncodings <- read.csv("D:/620/MUSA-620-Week-9-master/MUSA-620-Week-9-master/MUSA-620-Week-9-master/MUSA-620-Week-9-master/emoji-encodings.csv")
-#tweetTexts <- c("<ed><a0><bd><ed><b1><8d>nothing is impossible https://t.co/AckwyTxVjR","Yesterday was a huge day for our founders <e2><9c><8d><ef><b8><8f><ed><a0><be><ed><b7><a0><ed><a0><bd><ed><b2><a5> Thanks to the @MYOB and @BlueChilliGroup marketing and social media specialis<e2><80><a6>")
-
-emojiresults <- sapply(emojiEncodings$streamApiEncodings, regexpr, streamedtweetstext) %>%
+emojiresultschina <- sapply(emojiEncodings$streamApiEncodings, regexpr, streamedtweetschinatext) %>%
   data.frame()
-colnames(emojiresults) <- emojiEncodings$emojiDescription
+colnames(emojiresultschina) <- emojiEncodings$emojiDescription
 
 # StreamAPIencoding for REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S
 # <ed><a0><bc><ed><b7><ba><ed><a0><bc><ed><b7><b8>
@@ -175,30 +144,65 @@ colnames(emojiresults) <- emojiEncodings$emojiDescription
 #table(emojiresults["ANGRY FACE"])
 
 #Set no emoji to 0 but with emoji to 1
-emojiresults[emojiresults == -1] <- 0
-emojiresults[emojiresults != 0] <- 1
+emojiresultschina[emojiresultschina == -1] <- 0
+emojiresultschina[emojiresultschina != 0] <- 1
 
-China <- colSums(emojiresults)
-maga <- colSums(emojiresults)
+China <- colSums(emojiresultschina)
 
-# combine the total emoji of China and #maga in one data frame
-totalemoji <- cbind(China,maga) %>%
+
+###############For Key word "#maga"######################
+streamedtweets2 <- parseTweets("mytwitterstreammaga.json", verbose = FALSE)
+streamedtweets3 <- parseTweets("mytwitterstreammaga1.json", verbose = FALSE)
+
+# IMPORTANT - for the emojis to be properly encoded, you must use the line below
+streamedtweets2$text <- iconv(streamedtweets2$text , from = "latin1", to = "ascii", sub = "byte")
+streamedtweets3$text <- iconv(streamedtweets3$text , from = "latin1", to = "ascii", sub = "byte")
+
+streamedtweetsmaga <- rbind(streamedtweets2,streamedtweets3)
+
+# OPTIONAL - If you want to use the REST API (rtweet package) in your analysis, you should use this command to encode the emojis
+restAPI$text <- iconv(restAPI$text , from = "latin1", to = "ascii", sub = "byte")
+
+#filter the retweets
+streamedtweetsmaga <- streamedtweetsmaga %>%
+  filter(!str_detect(text, '^"')) %>%
+  filter(!str_detect(text, '^RT'))
+
+#Get the text of retweets 
+streamedtweetsmagatext <- streamedtweetsmaga$text
+streamedtweetsmaga<- dplyr::select(streamedtweets, source, text, created_at)
+
+# Matching multiple strings against a multiple keywords
+
+emojiresultsmaga <- sapply(emojiEncodings$streamApiEncodings, regexpr, streamedtweetsmagatext) %>%
   data.frame()
+colnames(emojiresultsmaga) <- emojiEncodings$emojiDescription
+
+#Set no emoji to 0 but with emoji to 1
+emojiresultsmaga[emojiresultsmaga == -1] <- 0
+emojiresultsmaga[emojiresultsmaga != 0] <- 1
+
+maga <- colSums(emojiresultsmaga)
+
+####################################################
+# combine the total emoji of China and #maga in one data frame
+#totalemoji <- cbind(China,maga) %>%
+#  data.frame()
 # Important!!! transform the rownames into the first columm or arrange is going to lose the emojis
-totalemojiname <- tibble::rownames_to_column(totalemoji, "emoji")
+#totalemojiname <- tibble::rownames_to_column(totalemoji, "emoji")
 # Sort descending and take the top 20 of US and CN share in common
-ChinaUS <- dplyr::arrange(totalemojiname,desc(China),desc(maga))%>%
-  head(20)
+#ChinaUS <- dplyr::arrange(totalemojiname,desc(China),desc(maga))%>%
+#  head(20)
 
 ###Pipe everything together: top 20 emojis
 top20emoji <- cbind(China,maga) %>%
   data.frame() %>%
   tibble::rownames_to_column("emoji") %>%
-  dplyr::arrange(desc(China),desc(maga))%>%
+  dplyr::arrange(desc(maga),desc(China))%>%
   head(20)
 
 ###Facet wrap top 20 emojis
-tallFormat = gather(top20emoji, key= country, value= frequency,China:maga)
+tallFormat = gather(top20emoji, key= keywords, value= frequency,China:maga)
 
 palette <- c('#70284a','#0d585f')
 
@@ -207,7 +211,7 @@ newlable <- c(
   `maga` = "#maga"
 )
 
-p <- ggplot(tallFormat,aes(x= country, y= frequency, fill = country)) +
+p <- ggplot(tallFormat,aes(x= keywords, y= frequency, fill = keywords)) +
   geom_bar(stat = "identity") +
   coord_flip() +
   ylab("value") +
@@ -217,23 +221,22 @@ p <- ggplot(tallFormat,aes(x= country, y= frequency, fill = country)) +
 ModelFacet <- p + facet_wrap(~emoji, ncol = 4)
 ModelFacet
 
-# Comparison 4: sentiment analysis
-
+################# word frequency analysis############################
 ##Take out all the tweet text with Emoji US(#maga)
-resultsUS <- resultsofemoji["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S"] %>%
-  cbind(streamedtweetstext) %>%
-  setnames(old=c("REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S","streamedtweetstext"), new=c("detectresult", "tweet")) %>%
+resultsUSmaga <- emojiresultsmaga["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S"] %>%
+  cbind(streamedtweetsmagatext) %>%
+  setnames(old=c("REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S","streamedtweetsmagatext"), new=c("detectresult", "tweet")) %>%
   mutate(detectresult = as.numeric(detectresult),
          text = as.character(tweet)) %>%
   dplyr::select(detectresult,text) %>%
   as.data.frame() %>%
-  dplyr::filter(detectresult!= -1) %>%
+  dplyr::filter(detectresult!= 0) %>%
   dplyr::select(text)
 
 ##Take out for all the tweet text with Emoji US(China)
-resultsUS <- emojiresults["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S"] %>%
-  cbind(streamedtweetstext) %>%
-  setnames(old=c("REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S","streamedtweetstext"), new=c("detectresult", "tweet")) %>%
+resultsUSchina <- emojiresultschina["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S"] %>%
+  cbind(streamedtweetschinatext) %>%
+  setnames(old=c("REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICATOR SYMBOL LETTER S","streamedtweetschinatext"), new=c("detectresult", "tweet")) %>%
   mutate(detectresult = as.numeric(detectresult),
          text = as.character(tweet)) %>%
   dplyr::select(detectresult,text) %>%
@@ -246,7 +249,8 @@ resultsUS <- emojiresults["REGIONAL INDICATOR SYMBOL LETTER U + REGIONAL INDICAT
 #str_replace()
 #regex alphanumeric, any alpha numeric characters between < and >
 reg <- "<[a-zA-Z0-9][a-zA-Z0-9]>"
-resultsUS$text <- gsub(reg," ",resultsUS$text)
+resultsUSmaga$text <- gsub(reg," ",resultsUSmaga$text)
+resultsUSchina$text <- gsub(reg," ",resultsUSchina$text)
 
 ###word frequency
 
@@ -262,33 +266,44 @@ reg <- "([^A-Za-z\\d#@']|'(?![A-Za-z\\d#@]))"
 # Stop words - common words that convey little meaning, should be removed
 stop_words$word
 
-resultsUSWords <- resultsUS %>%
-  filter(!str_detect(text, '^RT|^"')) %>%                                      # filter tweets starting w/ "rt"
+# word frequency #maga
+resultsUSmagaWords <- resultsUSmaga %>% 
+  filter(!str_detect(text, '^RT|^"')) %>%                                           # filter tweets starting w/ "rt"
   mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", "")) %>%   # remove links
   unnest_tokens(word, text, token = "regex", pattern = reg) %>%                     # parse the text into "tokens"
-  filter(!word %in% stop_words$word, str_detect(word, "[a-z]"))                     # remove stop words
+  filter(!word %in% stop_words$word, str_detect(word, "[a-z]")) %>%                 # remove stop words
+  filter(!word =="#maga")
 
-# word frequency #maga
-resultsUSWords %>%
+resultsUSmagaWords %>%
   count(word, sort = TRUE) %>%
   head(20) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
-  geom_bar(stat = "identity") +
+  geom_bar(stat = "identity",aes(fill = n)) +
+  scale_fill_gradient(low = "#7bbcb0", high = "#123f5a", name = "Occurrences \nof words") +
   ylab("Occurrences") +
   coord_flip() +
-  labs(title = "Word frequency of tweets talking about maga with US emoji")+plotTheme()
+  labs(title = "Word frequency of tweets with US emoji",subtitle = " when people are talking about #maga" )+plotTheme()
 
-# word frequency China
-resultsUSWords %>%
+# word frequency china
+resultsUSchinaWords <- resultsUSchina %>% 
+  filter(!str_detect(text, '^RT|^"')) %>%                                           # filter tweets starting w/ "rt"
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", "")) %>%   # remove links
+  unnest_tokens(word, text, token = "regex", pattern = reg) %>%                     # parse the text into "tokens"
+  filter(!word %in% stop_words$word, str_detect(word, "[a-z]")) %>%                 # remove stop words
+  filter(!word =="china")
+
+resultsUSchinaWords %>%
   count(word, sort = TRUE) %>%
   head(20) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
-  geom_bar(stat = "identity") +
+  geom_bar(stat = "identity",aes(fill = n)) +
+  scale_fill_gradient(low = "#ffc6c4", high = "#672044", name = "Occurrences \nof words") +
   ylab("Occurrences") +
   coord_flip() +
-  labs(title = "Word frequency of tweets talking about China with US emoji")+plotTheme()
+  labs(title = "Word frequency of tweets with US emoji",subtitle = " when people are talking about China" )+plotTheme()
+
 
 # but before analyzing the tweets, a primer on sentiment analysis using the syuzhet package
 # https://cran.r-project.org/web/packages/syuzhet/syuzhet.pdf
@@ -296,48 +311,68 @@ resultsUSWords %>%
 # of "good" and "bad" words. Good words get a positive score. Bad words get a negative score.
 # the average score across all words determines the overal sentiment.
 # calculate the positive/negative sentiment of tweets from each group
+
+
+# 1:00 a.m. to 5a.m. Mar.28th, 2018
 scores <- data.frame()
 
-score <- get_sentiment(resultsUSWords$word, method="syuzhet") %>%
+score <- get_sentiment(resultsUSmagaWords$word, method="syuzhet") %>%
   mean()
 scores <- rbind(scores,data.frame(keyword="#maga",score=score))
 
-score <- get_sentiment(resultsUSWords$word, method="syuzhet") %>%
+score <- get_sentiment(resultsUSchinaWords$word, method="syuzhet") %>%
   mean()
 scores <- rbind(scores,data.frame(keyword="China",score=score))
 
+# average sentiment scores
 ggplot(scores,aes(x=keyword, y=score, fill = score > 0)) +
   geom_bar(stat = "identity") +
   ylab("Sentiment") +
-  labs(title = "Sentiment analysis of tweets using US emoji", subtitle = "when people talk about China or #maga",caption = "using syuzhet package") +
-  scale_fill_manual(name = "", labels = c("Negative", "Positive"),
-                    values = c("red", "lightblue"))
+  labs(title = "Sentiment analysis of tweets using the 'US' emoji", subtitle = "when people talk about China or #maga \nbetween 1:00 a.m. to 5a.m. Mar.28th, 2018",caption = "using syuzhet package") +
+  scale_fill_manual(name = "", labels = c("Positive","Negative"),
+                    values = c("lightblue","red"))
 
 
+# 2:00 a.m. to 6a.m. Mar.27th, 2018
+scores <- data.frame()
+
+score <- get_sentiment(resultsUSmagaWords$word, method="syuzhet") %>%
+  mean()
+scores <- rbind(scores,data.frame(keyword="#maga",score=score))
+
+score <- get_sentiment(resultsUSchinaWords$word, method="syuzhet") %>%
+  mean()
+scores <- rbind(scores,data.frame(keyword="China",score=score))
+
+# average sentiment scores
+ggplot(scores,aes(x=keyword, y=score, fill = score > 0)) +
+  geom_bar(stat = "identity") +
+  ylab("Sentiment") +
+  labs(title = "Sentiment analysis of tweets using the 'US' emoji", subtitle = "when people talk about China or #maga \nbetween 2:00 a.m. to 6a.m. Mar.27th, 2018",caption = "using syuzhet package") +
+  scale_fill_manual(name = "", labels = c("Positive","Negative"),
+                    values = c("lightblue","red"))
 
 # calculate the NRC sentiment scores
 nrcScores <- data.frame()
 
-magaScores <- get_nrc_sentiment(resultsUSWords$word) %>%
+magaScores <- get_nrc_sentiment(resultsUSmagaWords$word) %>%
   summarise_all(funs(mean))
 nrcScores <- rbind(nrcScores,data.frame(keyword="#maga",magaScores))
 
-Chinascores <- get_nrc_sentiment(resultsUSWords$word) %>%
+Chinascores <- get_nrc_sentiment(resultsUSchinaWords$word) %>%
   summarise_all(funs(mean))
 nrcScores <- rbind(nrcScores,data.frame(keyword="China",Chinascores))
 
 
 tallFormat = gather(nrcScores, key=emotion, value=score, anger:positive)
 
-p <- ggplot(tallFormat,aes(x=keyword, y=score), fill = "black") +
-  geom_bar(stat = "identity") +
+p <- ggplot(tallFormat,aes(x=keyword, y=score)) +
+  geom_bar(stat = "identity",aes(fill = keyword)) +
   coord_flip() +
-  ylab("Sentiment") +
-  xlab("Keywords") +
-  labs(title = "NRC sentiment analysis of tweets using US emojis", subtitle = "when people talk about China or #maga", caption = "using syuzhet package")+
-  scale_fill_manual(name = score,labels = c("China","#maga"), values = c('#70284a','#0d585f'))+plotTheme()
+  ylab("Score of words") +
+  xlab("words") +
+  labs(title = "NRC sentiment analysis of tweets using US emojis", subtitle = "when people talk about China or #maga \nbetween 1:00 a.m. to 5a.m. Mar.28th, 2018", caption = "using syuzhet package")+
+  scale_fill_manual(labels = c("#maga","China"), values = c('#8dafc6','#e08f93'), name = "Score of words")+plotTheme()
 
 resultFacet <- p + facet_wrap(~emotion, ncol = 5)
 resultFacet
-
-
